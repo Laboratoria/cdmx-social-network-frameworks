a7ec08e07424132c024aa449f06b39541d00be82
BrisiaCastrejon <brissmonse@gmail.com>
2018-09-21 13:14:03 -0500

    Funcion para login con Google y dependencia para autenticacion

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'; // Importa React: Component
+// import logo from './logo.svg'; // Evita rutas relativas/estaticas
+import firebase from 'firebase';
+import './App.css'; // css del componente App
 
 class App extends Component {
+  login () {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesion`))
+      .catch(error => console.log(`${error.code}: ${error.message} Ha ocurrido un error`))
+  };
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h2 className="App-title">Red Social</h2>         
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>
+          <button onClick={this.login}>Google</button>
+        </p>        
       </div>
     );
   }
 }
-
 export default App;
--- social-network/src/App.test.js
+++ social-network/src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+// se renderiza el DIV sin romperse
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
