bd7ec6889970defb95f4dbb8a2ceb387da9ef88c
BrisiaCastrejon <brissmonse@gmail.com>
2018-09-21 14:19:54 -0500

    Funcion para inicio de sesion con Facebook y favicon

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -4,11 +4,18 @@ import firebase from 'firebase';
 import './App.css'; // css del componente App
 
 class App extends Component {
-  login () {
+  loginGoogle () {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider)
       .then(result => console.log(`${result.user.email} ha iniciado sesion`))
       .catch(error => console.log(`${error.code}: ${error.message} Ha ocurrido un error`))
+  };
+
+  loginFacebook () {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesion`))
+      .catch(error => console.log(`${error.code}: ${error.message} Ha ocurrido un error`))   
   };
   
   render() {
@@ -18,7 +25,10 @@ class App extends Component {
           <h2 className="App-title">Red Social</h2>         
         </header>
         <p>
-          <button onClick={this.login}>Google</button>
+          <button onClick={this.loginGoogle}>Google</button>
+        </p>
+        <p>
+          <button onClick={this.loginFacebook}>Facebook</button>
         </p>        
       </div>
     );
