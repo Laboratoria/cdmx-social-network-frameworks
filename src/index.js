82c8055469dcd0c90fe048d9307ed23f47457f21
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-09-26 12:07:17 -0500

    subiendo credenciales reutilizadas

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import './index.css';
 import App from './App';
 
 firebase.initializeApp({
-    apiKey: "AIzaSyCfn6b_XlLJBPuG6_6oWCUR4RxqA39JzSs",
-    authDomain: "greener-react.firebaseapp.com",
-    databaseURL: "https://greener-react.firebaseio.com",
-    projectId: "greener-react",
-    storageBucket: "greener-react.appspot.com",
-    messagingSenderId: "674937726579"
+  apiKey: 'AIzaSyCbW5lXCLbbrY6EkFROvAukVs8herq8G-Y',
+  authDomain: 'social-network-5b9ef.firebaseapp.com',
+  databaseURL: 'https://social-network-5b9ef.firebaseio.com',
+  projectId: 'social-network-5b9ef',
+  storageBucket: '',
+  messagingSenderId: '1074635944561'
   });
 
 
